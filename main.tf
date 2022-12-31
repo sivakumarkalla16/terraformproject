1ec8ed7631a95d53d2839a1e92f0740f0e68f17d
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-12-31 11:48:11 +0530

    huewhude

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ secret_key = "V/nOliSidARl+YAfCb3oX4D8L+9bqduM/UGnhgMc"
 
 resource "aws_instance" "ec2_example" {
   ami                    = "ami-0cca134ec43cf708f"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   tags = {
     Name = "Terraform EC2"
   }
